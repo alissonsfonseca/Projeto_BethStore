bc3a8cd64c00e10dd05f46a8868bb2d62441d837
GuilhermeVieiraMoraes <guims02@hotmail.com>
2022-05-27 19:41:31 -0300

    autentificação pt.1

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 import os
+from flask_login import LoginManager
 
 app = Flask(__name__)
 project_dir = os.path.dirname(os.path.abspath(__file__))
@@ -17,4 +18,12 @@ app.register_blueprint(autentificador, url_prefix='/')
 
 from app.models import tables
 db.create_all()
+
+login_manager = LoginManager()
+login_manager.login_view = 'autentificador.login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(id):
+    return tables.Usuario.query.get(int(id))
 
