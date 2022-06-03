daa2f3c75bbf7c3eacafc579838153cedac3c7dd
GuilhermeVieiraMoraes <guims02@hotmail.com>
2022-06-03 20:30:31 -0300

    cadastro de produto

--- app/controllers/produtoController.py
+++ app/controllers/produtoController.py
@@ -1,7 +1,7 @@
 from flask import Blueprint , render_template, redirect, url_for, request
 from flask_login import login_user, login_required, logout_user, current_user
 from app import db 
-from app.models.tables import Produto
+from app.models.tables import Produto, Categoria
 
 produtoController = Blueprint('produtoController', __name__)
 
@@ -9,6 +9,20 @@ produtoController = Blueprint('produtoController', __name__)
 @login_required
 def cadastroProduto():
     if current_user.admin == True:
-        return render_template('cadastroProduto.html', usuario = current_user)
+        categorias = Categoria.query.all()
+        if request.method == 'POST':
+            image = request.form.get('image')
+            marca = request.form.get('marca')
+            modelo = request.form.get('modelo')
+            preco = request.form.get('preco')
+            tamanho = request.form.get('tamanho')
+            quantidade = request.form.get('quantidade')
+            categoria = request.form.get('categoria')
+
+            novo_produto = Produto(imagem=image, marca=marca, modelo=modelo,preco=preco, tamanho=tamanho,
+            tamanho=tamanho, quantidade=quantidade,categoria=categoria)
+            db.session.add(novo_produto)
+            db.session.commit()
+        return render_template('cadastroProduto.html', usuario = current_user, categorias = categorias)
     else:
         return "Acesso apenas para admin"
