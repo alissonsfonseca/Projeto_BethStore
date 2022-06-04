2451d5f95b800efcb6d40fe0b5026915756a3afa
GuilhermeVieiraMoraes <guims02@hotmail.com>
2022-06-03 21:22:02 -0300

    categoria na navbar

--- app/controllers/controllers.py
+++ app/controllers/controllers.py
@@ -1,15 +1,16 @@
 from flask import Blueprint , render_template, redirect, url_for, request
 from flask_login import login_required, current_user
-from app.models.tables import Usuario
+from app.models.tables import Usuario, Produto, Categoria
 from app import db
-from app.models.tables import Produto 
 
 controllers = Blueprint('controllers', __name__)
 
 @controllers.route('/')
 def index():
     produtos = Produto.query.all()
-    return render_template('index.html', usuario=current_user, produtos=produtos)
+    categorias = Categoria.query.all()
+
+    return render_template('index.html', usuario=current_user, produtos=produtos, categorias=categorias)
 
 
 @controllers.route('/dashboard-usuario')
